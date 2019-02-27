04b1952e8e99dd30878701ec4a07af7969ef6de3
malinxiaM <770879122@qq.com>
2019-02-27 17:02:18 +0800

    框架已搭建

---
+++ api/severe.js
@@ -0,0 +1,17 @@
+//入口文件
+const express = require('express')
+const app = express()
+const path = require('path')
+//连接数据库
+const con = require('./db/connect')
+
+//基本插件
+//post 请求
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended:true}))
+//静态文件
+app.use('/public',express.static(path.join(__dirname,'./public')))
+
+app.listen(3000,()=>{
+    console.log('severe start in port:' + 3000)
+})
